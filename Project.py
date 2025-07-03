37dd1fe9a7ecf218cfa483bf77695adab7e29b9e
FJCRPV <62481723+FJCRPV@users.noreply.github.com>
2025-07-03 16:37:27 +0100

    Add files via upload

---
+++ Project.py
@@ -0,0 +1,19 @@
+answers = {
+    "name": "<Your Name>",
+    "student_id": "<Your Student ID>",
+    "a1":  "", # wallet address
+    "a2":  "", # transaction hash
+    "a3":  "", # free text answer to question 4
+    "a4":  "", # transaction hash
+    "a5":  "", # transaction hash
+    "a6":  "", # transaction hash
+    "a7":  "", # free text answer to question 7
+    "a8":  "", # transaction hash
+    "a9":  "", # transaction hash
+    "a10": "", # transaction hash
+    "a11": "", # transaction hash
+    "a12": "", # transaction hash
+    "a13": "", # free text answer to question 13
+    "a14": "", # transaction hash
+    "a15": "", # transaction hash    
+}
